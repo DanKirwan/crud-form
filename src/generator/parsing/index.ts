b21c1c02f7c5528b58e58ba7d0c3c8baa6d8282b
Dan Kirwan <dan.kirwan20@imperial.ac.uk>
2025-03-06 17:10:32 +0000

    added initial openapi parsing and generation tests

---
+++ src/generator/parsing/index.ts
@@ -0,0 +1,79 @@
+import { OpenAPIV3, OpenAPIV3_1 } from 'openapi-types';
+
+export const methods: `${OpenAPIV3.HttpMethods}`[] = Object.values(
+  OpenAPIV3.HttpMethods,
+);
+export type Method = (typeof methods)[number];
+
+export const validSchemes = ['https', 'http', 'ws', 'wss'] as const;
+export type Scheme = (typeof validSchemes)[number];
+
+export type PathSeg =
+  | { type: 'literal'; value: string }
+  | { type: 'parameter'; name: string };
+
+export type BaseRequestConfig = {
+  pathSegs: PathSeg[];
+  method: Method;
+};
+
+export type OasDocument = OpenAPIV3.Document | OpenAPIV3_1.Document;
+
+export type OasServerInfo = {
+  scheme?: Scheme | undefined;
+  host?: string | undefined;
+  basePath: PathSeg[];
+};
+
+type OasQueryParam = {
+  name: string;
+  required: boolean;
+};
+
+export type OasRequestConfig = BaseRequestConfig & {
+  servers: OasServerInfo[];
+  queryParameters: OasQueryParam[];
+};
+
+export type DocRequestConfig = BaseRequestConfig & {
+  originalPath: string;
+  scheme?: Scheme;
+  host?: string;
+  queryParameters: { name: string; value: string }[];
+  line: number;
+};
+
+export type Conflict =
+  | {
+      type: 'path-parameter-name-mismatch';
+      parameterIndex: number;
+      oasServerIndex: number | null;
+    }
+  | {
+      type: 'method-mismatch';
+    }
+  | {
+      type: 'host-mismatch';
+      oasHost: OasRequestConfig['servers'][number]['host'];
+    }
+  | {
+      type: 'doc-scheme-not-supported-by-oas-server';
+    };
+
+export type Inconsistency =
+  | {
+      type: 'only-in-doc';
+      requestConfig: DocRequestConfig;
+    }
+  | {
+      type: 'only-in-oas';
+      requestConfig: OasRequestConfig;
+    }
+  | {
+      type: 'match-with-conflicts';
+      oasRequestConfig: OasRequestConfig;
+      docRequestConfig: DocRequestConfig;
+      conflicts: Conflict[];
+    };
+
+export type FailOutput = Inconsistency[];
---
+++ src/generator/parsing/openapi.ts
@@ -0,0 +1,140 @@
+import SwaggerParser from '@apidevtools/swagger-parser';
+import { includes, isNumber } from 'lodash-es';
+import { OpenAPI, OpenAPIV2, OpenAPIV3 } from 'openapi-types';
+import { convertFile } from 'swagger2openapi';
+import {
+  OasDocument,
+  OasRequestConfig,
+  OasServerInfo,
+  Scheme,
+  methods,
+  validSchemes,
+} from './';
+import { objectEntries } from '@src/lib/objectUtils';
+
+const isV2 = (openapiDoc: OpenAPI.Document): openapiDoc is OpenAPIV2.Document =>
+  !!(openapiDoc as Partial<Pick<OpenAPIV2.Document, 'swagger'>>).swagger;
+
+const oasParseServers = (servers?: OasDocument['servers']) => {
+  if (!servers) return [];
+  return servers.map<OasServerInfo>(s => {
+    // if (s.variables) throw new Error('Server variables not supported yet');
+    try {
+      const url = new URL(s.url);
+      const protocol = url.protocol.replace(':', '');
+      return {
+        scheme: includes(validSchemes, protocol)
+          ? (protocol as Scheme)
+          : void 0,
+        basePath: oasParsePath(url.pathname),
+        host: url.host,
+      };
+    } catch (_) {
+      const protocol = s.url.includes('://') ? s.url.split('://')[0] : void 0;
+      return {
+        scheme: includes(validSchemes, protocol)
+          ? (protocol as Scheme)
+          : void 0,
+        basePath: oasParsePath(s.url.match(/(?<!\/)\/.+/g)?.[0]),
+        host: s.url.match(/(?<=\/\/)(.*?)(?=\/|$)/g)?.[0] ?? void 0,
+      };
+    }
+  });
+};
+
+const isRefObject = (
+  obj: OpenAPI.Parameter,
+): obj is OpenAPIV3.ReferenceObject => Object.hasOwn(obj, '$ref');
+
+const oasParseQueryParams = (parameters: OpenAPI.Parameters) => {
+  const queryParameters: OasRequestConfig['queryParameters'] = [];
+  for (const p of parameters) {
+    if (isRefObject(p)) {
+      throw new Error('Parameters should have been dereferenced');
+    }
+    if (p.in !== 'query') continue;
+    queryParameters.push({
+      name: p.name,
+      required: p.required ?? false,
+    });
+  }
+  return queryParameters;
+};
+
+export const oasParsePath = (path?: string) => {
+  const pathSegs: OasRequestConfig['pathSegs'] = [];
+  if (!path) return pathSegs;
+  if (!path.startsWith('/')) throw new Error('Path must start with /');
+  path = path.substring(1);
+  if (path === '') return pathSegs;
+  for (const part of path.split('/')) {
+    switch (true) {
+      case part.startsWith('{') && part.endsWith('}'):
+        pathSegs.push({
+          type: 'parameter',
+          name: part.substring(1, part.length - 1),
+        });
+        break;
+      default:
+        pathSegs.push({ type: 'literal', value: part });
+        break;
+    }
+  }
+  return pathSegs;
+};
+
+export const oasParseRequestConfigs = (oas: OasDocument) => {
+  const oasIdToRequestConfig = new Map<string, OasRequestConfig>();
+  if (!oas.paths) return oasIdToRequestConfig;
+  const oasServers = oasParseServers(oas.servers);
+  for (const [path, pathItem] of objectEntries(oas.paths)) {
+    if (!pathItem || isNumber(path)) continue;
+    for (const method of methods) {
+      const requestConfigItem = pathItem[method];
+      if (!requestConfigItem) continue;
+
+      const pathSegs = oasParsePath(path);
+
+      const servers = requestConfigItem.servers
+        ? oasParseServers(requestConfigItem.servers)
+        : pathItem.servers
+          ? oasParseServers(pathItem.servers)
+          : oasServers;
+
+      const queryParameters = oasParseQueryParams(
+        requestConfigItem.parameters ?? [],
+      );
+
+      oasIdToRequestConfig.set(`${method} ${path}`, {
+        method,
+        servers,
+        pathSegs,
+        queryParameters,
+      });
+    }
+  }
+  return oasIdToRequestConfig;
+};
+
+export const oasParse = async (oasPath: string, shouldDereference = true) => {
+  const oasDoc = shouldDereference
+    ? await SwaggerParser.dereference(oasPath)
+    : await SwaggerParser.bundle(oasPath);
+  const oas = isV2(oasDoc) ? (await convertFile(oasPath, {})).openapi : oasDoc;
+  return oas;
+};
+
+export const oasPathSegsToPath = (pathSegs: OasRequestConfig['pathSegs']) => {
+  let path = '';
+  for (const part of pathSegs) {
+    switch (part.type) {
+      case 'parameter':
+        path += `/{${part.name}}`;
+        break;
+      case 'literal':
+        path += `/${part.value}`;
+        break;
+    }
+  }
+  return path === '' ? '/' : path;
+};
